ac26eedbd49efbdf61d9eeb71ffb99aecc9ee9f2
djulfikar <muhammaddjulfikar7@gmail.com>
2018-10-29 23:23:50 +0700

    PBO3-10117086-Latihan62-LivingThing

---
+++ src/pbo3/pkg10117086/latihan62/livingthing/Human.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117086.latihan62.livingthing;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Human extends LivingThing {
+    
+    private String nama;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    
+    @Override
+    public void walk(String nama){
+        System.out.println("Sedang Berjalan");
+    }
+    
+}
